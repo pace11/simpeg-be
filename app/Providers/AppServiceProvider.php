e099773cde86a9f46c2165d2cd47e631346c5ad6
pace11 <ryanjoker87@gmail.com>
2023-12-25 19:10:50 +0700

    feat: deploy vercel

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,6 +26,6 @@ class AppServiceProvider extends ServiceProvider
     {
         if(env('APP_ENV') !== 'local') {
             URL::forceScheme('https');
-         }
+        }
     }
 }
