e051fe6a040fd27efebf16bc8e42fe3a17ef518f
pace11 <ryanjoker87@gmail.com>
2023-12-25 19:14:11 +0700

    feat: deploy vercel

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -12,7 +12,7 @@ return [
                 /*
                  * Route for accessing api documentation interface
                 */
-                'api' => 'v2/docs',
+                'api' => 'api/docs',
             ],
             'paths' => [
                 /*
